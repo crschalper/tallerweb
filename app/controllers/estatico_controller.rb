d120b8f1a5cf2525c28ae90bdf169dfc33a67630
Cristian Schalper <crschalper@gmail.com>
2011-09-11 18:54:06 -0400

    Agregando username,nombre y apellido. v.0.2

--- app/controllers/estatico_controller.rb
+++ app/controllers/estatico_controller.rb
@@ -1,4 +1,5 @@
 class EstaticoController < ApplicationController
+before_filter :authenticate_user!, :except => [:index]
   def index
   end
   
--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Webapp::Application.initialize!
 
-Rails::Initializer.run do |config|
-	
-  # Configure application for use with ambethia-recaptcha gem
-  config.gem "ambethia-recaptcha", :lib => "recaptcha/rails", :source => "http://gems.github.com"
-end
